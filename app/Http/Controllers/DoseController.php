1eba63f0bdf4a3f692fb3bf405b4713145414cae
M. Kamrul H <mkamrulh1st@gmail.com>
2022-07-07 19:29:56 +0600

    app

--- app/Http/Controllers/DoseController.php
+++ app/Http/Controllers/DoseController.php
@@ -7,58 +7,29 @@ use Illuminate\Http\Request;
 
 class DoseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $doeses = Dose::query()->latest()->limit(12)->get();
         return view('dose.dose',compact('doeses'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request ,['name'=>'required|unique:doses']);
         Dose::query()->create(['name'=>$request->name]);
-        $doeses = Dose::query()->latest()->limit(12)->get();
         return redirect(route('dose.index'))->with('success', 'Dose has been deleted successfully');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Dose  $does
-     * @return \Illuminate\Http\Response
-     */
     public function show(Dose $does)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Dose  $does
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $doeses = Dose::query()->latest()->limit(12)->get();
@@ -66,13 +37,6 @@ class DoseController extends Controller
         return view('dose.dose',compact('does','id','doeses'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Dose  $does
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $this->validate($request ,['name'=>'required']);
@@ -81,12 +45,6 @@ class DoseController extends Controller
         return redirect(route('dose.index'))->with('success', 'Dose has been deleted successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Dose  $does
-     * @return \Illuminate\Http\Response
-     */
     public function delete($id)
     {
         $data = Dose::query()->find($id);
